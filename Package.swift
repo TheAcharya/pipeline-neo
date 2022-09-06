4348121ef71ed84e18a5cfd2dbbf57a41f8fad32
vzhd1701 <vzhd1701@gmail.com>
2022-09-06 15:41:55 +0500

    build: convert XCode project into Swift package

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "Pipeline",
+    products: [
+        .library(
+            name: "Pipeline",
+            targets: ["Pipeline"]),
+    ],
+    targets: [
+        .target(
+            name: "Pipeline",
+            dependencies: []),
+    ]
+)
